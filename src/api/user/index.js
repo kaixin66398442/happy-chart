edaa97ddfa153fef825adb9a331721d84d53b1f7
happy <lkx13537544672@163.com>
2026-01-08 15:18:54 +0800

    fix: 修复用户接口基础URL末尾多余的斜杠

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -4,7 +4,7 @@ import request from "@/utils/request";
 
 // user相关接口
 // const baseURL = 'http://127.0.0.1:3007' // 本地
-const baseURL = 'https://happy-chart-server.vercel.app/' // vercel
+const baseURL = 'https://happy-chart-server.vercel.app' // vercel
 // 登录
 const loginURL = baseURL + '/api/login'
 // 注册
